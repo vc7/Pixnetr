63e49e2db899b40ff67f8eeb526be004c8fde659
Vincent Chen <vince78718@gmail.com>
2014-07-29 14:04:10 +0800

    Initial Commit

---
+++ Pixnetr/PXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PXAppDelegate.h
+//  Pixnetr
+//
+//  Created by vincent on 2014/07/29.
+//  Copyright (c) 2014年 Vincent Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pixnetr/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pixnetr
+//
+//  Created by vincent on 2014/07/29.
+//  Copyright (c) 2014年 Vincent Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PXAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PXAppDelegate class]));
+    }
+}
