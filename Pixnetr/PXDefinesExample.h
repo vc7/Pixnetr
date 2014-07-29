b8185a3d21eada00250cb91a949db82a364c7e2e
Vincent Chen <vince78718@gmail.com>
2014-07-29 15:01:38 +0800

    Adding defines file template for api keys

---
+++ Pixnetr/PXDefinesExample.h
@@ -0,0 +1,20 @@
+//
+//  PXDefinesExample.h
+//  Pixnetr iOS App
+//
+//  Created by Vincent Chen on 7/29/14.
+//  Copyright (c) 2014 Vincent Chen. All rights reserved.
+//
+
+#ifndef PXDEFINES
+#define PXDEFINES
+
+#pragma mark - API - Parse
+
+extern NSString *const kPXParseApplicationID;
+extern NSString *const kPXParseClientKey;
+
+#pragma mark - API - Pixnet
+
+extern NSString *const kPXPixnetConsumerKey;
+extern NSString *const kPXPixnetConsumerSecret;
---
+++ Pixnetr/PXDefinesExample.m
@@ -0,0 +1,27 @@
+//
+//  PXDefinesExample.h
+//  Pixnetr iOS App
+//
+//  Created by Vincent Chen on 7/29/14.
+//  Copyright (c) 2014 Vincent Chen. All rights reserved.
+//
+
+#import "PXDefines.h"
+
+//
+// Remove the line that starts with `#error` after you have filled
+// in your API credentials.
+//
+
+#pragma mark - API - Parse
+#error You need to fill in PXDefines.m with your Parse service credentials
+
+NSString *const kPXParseApplicationID = <# Parse App ID #>;
+NSString *const kPXParseClientKey = <#Parse Client Key#>;
+
+#pragma mark - API - Pixnet
+#error You need to fill in PXDefines.m with your Pixnet service credentials
+
+NSString *const kPXPixnetConsumerKey = <#Pixnet Consumer Key#>;
+NSString *const kPXPixnetConsumerSecret = <#Pixnet Consumer Secret#>;
+
