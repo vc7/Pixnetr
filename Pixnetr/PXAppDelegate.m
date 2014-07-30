42b0af8b2b894171206000f7539b542448a393b8
Vincent Chen <vince78718@gmail.com>
2014-07-30 16:16:58 +0800

    Adding setup api keys.

--- Pixnetr/PXAppDelegate.m
+++ Pixnetr/PXAppDelegate.m
@@ -6,7 +6,10 @@
 //  Copyright (c) 2014年 Vincent Chen. All rights reserved.
 //
 
+#import <PXKService.h>
+
 #import "PXAppDelegate.h"
+#import "PXDefines.h"
 
 @implementation PXAppDelegate
 
@@ -18,36 +21,19 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    // Bootstrap the Services
+    [self _bootstrapServices];
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
+- (void)_bootstrapServices
 {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    [PXKService setParseApplicationId:kPXParseApplicationID clientKey:kPXParseClientKey];
+    [PXKService setPixnetConsumerKey:kPXPixnetConsumerKey consumerSecret:kPXPixnetConsumerSecret callbackURL:@"http://devtool.pixnet.pro/index/cb"];
 }
 
 @end
