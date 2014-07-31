bc992abe826020ff573f028c7e6b947c9e79953b
Vincent Chen <vince78718@gmail.com>
2014-08-01 07:32:19 +0800

    Add light box to display single given image.

---
+++ Pixnetr/PXPhotoLightBoxViewController.h
@@ -0,0 +1,15 @@
+//
+//  PXPhotoLightBoxViewController.h
+//  Pixnetr
+//
+//  Created by vincent on 2014/08/01.
+//  Copyright (c) 2014年 Vincent Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PXPhotoLightBoxViewController : UITableViewController
+
+@property (strong, nonatomic) PXKPhoto *photo;
+
+@end
