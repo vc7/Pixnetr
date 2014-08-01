d031fb40e56bb4cdb8632973d5f2b083e0aadd03
Vincent Chen <vince78718@gmail.com>
2014-08-01 13:38:53 +0800

    Implement Light Box Controller.

--- Pixnetr/PXPhotoLightBoxViewController.m
+++ Pixnetr/PXPhotoLightBoxViewController.m
@@ -6,7 +6,12 @@
 //  Copyright (c) 2014年 Vincent Chen. All rights reserved.
 //
 
+#import <SDWebImage/SDWebImageManager.h>
+
 #import "PXPhotoLightBoxViewController.h"
+#import "PXPhotoLightBoxMainTableViewCell.h"
+
+static NSString *PhotoCellIdentifier = @"PhotoCellIdentifier";
 
 @interface PXPhotoLightBoxViewController ()
 
@@ -28,6 +33,7 @@
     [super viewDidLoad];
     
     self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -40,76 +46,64 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
+    return 1;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row == 0) {
+        PXPhotoLightBoxMainTableViewCell *cell = (PXPhotoLightBoxMainTableViewCell *)[tableView dequeueReusableCellWithIdentifier:PhotoCellIdentifier];
+        
+        if (cell == nil) {
+            cell = [[PXPhotoLightBoxMainTableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:PhotoCellIdentifier];
+        }
+        
+        cell.mainImageView.image = nil;
+        cell.mainImageView.backgroundColor = [UIColor lightGrayColor];
+        cell.photoRatio = [self.photo.photoRatio floatValue];
+        
+        [[SDWebImageManager sharedManager] downloadImageWithURL:[NSURL URLWithString:[PXKURLGenerator generatePhotoImageURLStringWithPhoto:self.photo size:(CGSize){ 640, 640/[self.photo.photoRatio floatValue] }]]
+                                                        options:0
+                                                       progress:^(NSInteger receivedSize, NSInteger expectedSize) {
+                                                       }
+                                                      completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
+                                                          if ( ! error) {
+                                                              cell.mainImageView.image = image;
+                                                          }
+                                                      }];
+        return cell;
+
+        
+    } else {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:PhotoCellIdentifier forIndexPath:indexPath];
+        
+        // Configure the cell...
+        
+        return cell;
+    }
+}
+
+#pragma mark - UITableViewDelegate
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row == 0) {
+        return CGRectGetWidth(self.tableView.frame)/[self.photo.photoRatio floatValue];
+    }
     return 0;
 }
 
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
-    return cell;
+    NSLog(@"%@", indexPath);
 }
-*/
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
